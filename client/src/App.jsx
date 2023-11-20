26f0b8cc20d1ed5bb9252fd4c52100c785056923
Debdut karpas <nilkarpas@gmail.com>
2023-11-20 23:39:19 +0530

    setting up client

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from "react-router-dom"
+import AuthLayout from "./_auth/AuthLayout"
+import RootLayout from "./_root/RootLayout"
+import SignupForm from "./_auth/forms/SignupForm"
+import SigninForm from "./_auth/forms/SigninForm"
+import Home from "./_root/pages/Home"
+
+import "./global.css"
+
+function App() {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public route */}
+        <Route element={<AuthLayout/>}>
+          <Route path="/sign-up" element={<SignupForm/>}/>
+          <Route path="/sign-in" element={<SigninForm/>}/>
+        </Route>
+
+        {/* Private route */}
+        <Route element={<RootLayout/>}>
+          <Route path="/" element={<Home/>}/>
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
