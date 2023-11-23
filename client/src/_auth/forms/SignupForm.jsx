7ac7d1b848821ec361379bf8c1f9e21cc731bc4e
Debdut karpas <nilkarpas@gmail.com>
2023-11-24 00:08:49 +0530

    sign-up ui

--- client/src/_auth/forms/SignupForm.jsx
+++ client/src/_auth/forms/SignupForm.jsx
@@ -1,19 +1,20 @@
-// import React from 'react'
+"use client"
+ 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
-import { Button } from "../../components/ui/button"
+ 
+import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "../../components/ui/form"
-import { Input } from "../../components/ui/input"
-
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+ 
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
@@ -21,14 +22,17 @@ const formSchema = z.object({
 })
 
 const SignupForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      name : "",
       username: "",
+      email : "",
+      password : "",
     },
   })
 
-  function handleSignup(values) {
+  function onSubmit(values) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
@@ -36,13 +40,32 @@ const SignupForm = () => {
 
   return (
     <Form {...form}>
-      <section>
+      <section className="sm:w-420 flex-center flex-col">
         <img src="../../../public/images/logo.svg" alt="logo"/>
-        <h2>Create a new account</h2>
-        <p>To use Snapgram enter your details</p>
-        {/* <form
-          onSubmit={form.handleSubmit(handleSignup)}
-          className="flex flex-col gap-5 w-full mt-4">
+        <h2 
+          className="
+            h3-bold 
+            md:h2-bold 
+            pt-5 
+            sm:pt-12
+          "
+        >
+          Create a new account
+        </h2>
+        <p 
+          className="
+            text-light-3
+            small-medium
+            md:base-regular
+            mt-2
+          "
+        >
+          To use Snapgram enter your details
+        </p>
+        <form 
+          onSubmit={form.handleSubmit(onSubmit)} 
+          className="flex flex-col gap-5 mt-4 w-full"
+        >
           <FormField
             control={form.control}
             name="name"
@@ -56,7 +79,55 @@ const SignupForm = () => {
               </FormItem>
             )}
           />
-        </form> */}
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input"  {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit" 
+            className="shad-button_primary"
+          >
+            Sign Up
+          </Button>
+      </form>
       </section>
     </Form>
   )
