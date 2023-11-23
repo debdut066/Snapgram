2b32c896ea22e4de48e8d583384db4257e4176b7
Debdut karpas <nilkarpas@gmail.com>
2023-11-24 02:42:04 +0530

    icons and backend

---
+++ server/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const createError = require("http-errors");
+const fileUpload = require("express-fileupload");
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+app.use(cors());
+app.use(fileUpload({
+    limits : { fileSize : 50 * 1024 * 1024 },
+    useTempFiles : true,
+    tempFileDir : "/tmp/"
+}))
+
+/**
+* @PUBLIC_ROUTE
+*/
+
+// If route not found
+app.use(async (req, res, next)=>{
+    next(createError.NotFound("Page not found"))
+})
+
+// Error message
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error : {
+            status : err.status || 500,
+            message : err.message
+        }
+    })
+})
+
+const server = app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}`)
+})
