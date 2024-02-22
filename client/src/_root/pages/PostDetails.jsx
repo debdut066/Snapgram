4cedeb68b011df09b18d5e884fe793da2b492f9d
Debdut karpas <nilkarpas@gmail.com>
2024-02-22 23:59:01 +0530

    refactor: post details page

--- client/src/_root/pages/PostDetails.jsx
+++ client/src/_root/pages/PostDetails.jsx
@@ -1,43 +1,60 @@
 import { useParams, useNavigate, Link } from "react-router-dom"
 
+import { FiSend } from "react-icons/fi";
+
 import { Button } from "../../components/ui/button";
 import Loader from "../../components/shared/Loader"
 import { UserContext } from "../../context/AuthContext"
 import { multiFormatDateString } from "../../lib/utils";
 
-import { useDeletePost, useGetPostById } from "../../lib/react-query/queries"; 
+import { useDeletePost, useGetPostById, useCreateComment } from "../../lib/react-query/queries"; 
 import PostStats from "../../components/shared/PostStats";
+import CommentList from "../../components/shared/CommentList"
+import { useState } from "react";
 
 export default function PostDetails(){
     const navigate = useNavigate();
     const { id : postId } = useParams();
     const { token, user } = UserContext();
+    const [commentValue, setCommentValue] = useState("")
 
     const { data : post, isLoading } = useGetPostById(token, postId);
     const { mutate : deletePost } = useDeletePost();
+    const { mutateAsync: createComment, isLoading: isCreatingComment } = useCreateComment();
 
     function handleDeletePost(){
         deletePost({ postId : postId, token : token })
         navigate(-1);
     }
+
+    async function submitComment(){
+        let reqBody = {
+            content : commentValue,
+            postId : post._id
+        }
+        await createComment({ data : reqBody, token : token });
+        setCommentValue("")
+    }
+
+    console.log("data", post)
     
     return (
         <div className="post_details-container">
-            {/* <div className="hidden md:flex max-w-5xl w-full">
+            <div className="hidden md:flex max-w-5xl w-full">
                 <Button
                     onClick={()=>navigate(-1)}
                     variant="ghost"
                     className="shad-button_ghost"
                 >
                     <img
-                        src="../../../public/icons/back.svg"
+                        src="../../../icons/back.svg"
                         alt="back"
                         width={24}
                         height={24} 
                     />
                     <p className="small-medium lg:base-medium">Back</p>
                 </Button>
-            </div> */}
+            </div>
 
             {isLoading && !post ? (
                 <Loader/>
@@ -51,30 +68,43 @@ export default function PostDetails(){
 
                     <div className="post_details-info">
                         <div className="flex-between w-full">
-                            <Link
-                                to={`/profile/${post?.creator._id}`}
-                                className="flex items-center gap-3"
-                            >
-                                <img
-                                    src={post?.creator.imageUrl || "../../../icons/profile-placeholder.svg"} 
-                                    alt="creator"
-                                    className="w-8 h-8 lg:w-12 lg:h-12 rounded-full"
-                                />
-                                <div className="flex gap-1 flex-col">
-                                    <p className="base-medium lg:body-bold text-light-1">
-                                        {post?.creator.name}
-                                    </p>
-                                    <div className="flex-center gap-2 text-light-3">
-                                        <p className="subtle-semibold lg:small-regular">
-                                            {multiFormatDateString(post.createdAt)}
+                            <div className="flex-col flex-1 w-full small-medium lg:base-regular">
+                                <Link
+                                    to={`/profile/${post?.creator._id}`}
+                                    className="flex items-center gap-3"
+                                >
+                                    <img
+                                        src={post?.creator.imageUrl || "../../../icons/profile-placeholder.svg"} 
+                                        alt="creator"
+                                        className="w-8 h-8 lg:w-12 lg:h-12 rounded-full"
+                                    />
+                                    <div className="flex gap-1 flex-col">
+                                        <p className="base-medium lg:body-bold text-light-1">
+                                            {post?.creator.name}
                                         </p>
-                                        -
-                                        <p className="subtle-semibold lg:small-regular">
-                                            {post.location}
-                                        </p>
+                                        <div className="flex-center gap-2 text-light-3">
+                                            <p className="subtle-semibold lg:small-regular">
+                                                {multiFormatDateString(post.createdAt)}
+                                            </p>
+                                            -
+                                            <p className="subtle-semibold lg:small-regular">
+                                                {post.location}
+                                            </p>
+                                        </div>
                                     </div>
+                                </Link>
+
+                                <div className="flex flex-1 w-full items-baseline mt-5 gap-x-2">
+                                    <p>{post.caption}</p>
+                                    <ul className="flex gap-1 mt-2">
+                                        {post.tags[0].split(',').map((tag,index)=>(
+                                            <li key={`${tag}${index}`} className="text-light-3 small-regular">
+                                                #{tag}
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            </Link>
+                            </div>
 
                             <div className="flex-center gap-4">
                                 <Link
@@ -91,7 +121,7 @@ export default function PostDetails(){
                                 <Button
                                     onClick={handleDeletePost}
                                     variant="ghost"
-                                    className={`ost_details-delete_btn ${user._id !== post.creator._id && "hidden"}`}
+                                    className={`post_details-delete_btn ${user._id !== post.creator._id && "hidden"}`}
                                 >
                                     <img
                                         src="../../../icons/delete.svg"
@@ -100,26 +130,35 @@ export default function PostDetails(){
                                         width={24}
                                     />
                                 </Button>
+                                
                             </div>   
                         </div>
 
                         <hr className="border w-full border-dark-4/80" />
+                        
+                        <CommentList commentData={post.comment}/>
 
-                        <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
-                            <p>{post.caption}</p>
-                            <ul className="flex gap-1 mt-2">
-                                {post.tags.map((tag,index)=>(
-                                    <li key={`${tag}${index}`} className="text-light-3 small-regular">
-                                        #{tag}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-
-                        <div className="w-full">
+                        <div className="w-full h-24 flex flex-col gap-x-10 justify-between">
                             <PostStats post={post} user={user} token={token}/>
+                            <div className="h-2/5 flex justify-center items-center w-full gap-x-5">
+                                <img 
+                                    src={user.imageUrl || "../../../icons/profile-placeholder.svg"} 
+                                    alt="profile-image"
+                                    className="h-8 w-8 rounded-full"
+                                />
+                                <span className="flex px-5 justify-center items-center w-full rounded bg-dark-4">
+                                    <input 
+                                        value={commentValue}
+                                        placeholder="Write your comment..."
+                                        onChange={(e)=>setCommentValue(e.target.value)}
+                                        className="bg-dark-4 h-10 placeholder:text-light-4 focus:outline-none w-full"
+                                    />
+                                    <button onClick={submitComment} disabled={isCreatingComment}>
+                                        <FiSend color="#fbbf24" size={20}/>
+                                    </button>
+                                </span>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             )}
--- client/src/_root/pages/SavedPosts.jsx
+++ client/src/_root/pages/SavedPosts.jsx
@@ -1,19 +1,35 @@
 import Loader from "../../components/shared/Loader";
 import { UserContext } from "../../context/AuthContext";
 import { useGetSavedPost } from "../../lib/react-query/queries"
+import GridPostList from "../../components/shared/GridPostList";
 
 
 export default function SavedPosts(){
 	const { token } = UserContext();
-	const { data : savedPost, isLoading : isPostLoading, } = useGetSavedPost(token)
-
-	if(isPostLoading){
-		<Loader/>
-	}
-
-	console.log("post", savedPost)
-
+	const { data : savedPosts, isLoading : isPostLoading, } = useGetSavedPost(token)
 	return(
-		<div>SavedPost</div>
+		<div className="saved-container">
+			<div className="flex gap-2 w-full max-5-xl">
+				<img
+					src="../../../icons/save.svg"
+					alt="save"
+					width={36}
+					height={36}
+					className="invert-white"
+				/>
+				<h2 className="h3-bold md:h2-bold text-left w-full">Saved Post</h2>
+			</div>
+			{isPostLoading ? (
+				<Loader/>
+			) : (
+				<ul className="w-full flex justify-center max-w-5xl gap-9">
+					{savedPosts.length === 0 ? (
+						<p>No post available</p>
+					) : (
+						<GridPostList posts={savedPosts.saved} showStats={true}/>
+					)}
+				</ul>
+			)}
+		</div>
 	)
 }
